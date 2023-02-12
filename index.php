c055fdcb77edb0511873c8cdc9a9cc2139764010
Tharindu Panagoda <tpanagoda@gmail.com>
2023-02-12 19:46:37 +0530

    index.php added db connection

--- index.php
+++ index.php
@@ -5,5 +5,29 @@
 </head>
 <body>
 <h1><?php echo "Hello World" ?></h1>
+<p>
+    <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Create database
+    $sql = "CREATE DATABASE myDB";
+    if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+    } else {
+    echo "Error creating database: " . $conn->error;
+    }
+
+    $conn->close();
+    ?>
+</p>
 </body>
 </html>
