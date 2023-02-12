3e385bc8bb9384eb98db46a3ac26a0b1fe4ed077
Tharindu Panagoda <tpanagoda@gmail.com>
2023-02-12 21:48:52 +0530

    index.php fixed the db connection issue

--- form.php
+++ form.php
@@ -22,6 +22,23 @@
         die("Connection failed!" . mysqli_connect_error());
     }
 
+    // sql to create table
+  $sql = "CREATE TABLE IF NOT EXISTS contactform_entries (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+  } else {
+    echo "Error creating table: " . $conn->error;
+  }
+  
+  $conn->close();
+
     // using sql to create a data entry query
     $sql = "INSERT INTO contactform_entries (id, fname, lname, email) VALUES ('0', '$fname', '$lname', '$email')";
   
