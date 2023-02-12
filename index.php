e1d33ec69490b90fb4b9464faceffd7c95c5163b
Tharindu Panagoda <tpanagoda@gmail.com>
2023-02-12 19:41:21 +0530

    index.php file changed

--- index.php
+++ index.php
@@ -1,26 +1,9 @@
-
-<h1>
-    Webpage loaded!!!!
-</h1>
-<?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . $conn->error;
-}
-
-$conn->close();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Embed PHP in a .html File</title>
+</head>
+<body>
+<h1><?php echo "Hello World" ?></h1>
+</body>
+</html>
